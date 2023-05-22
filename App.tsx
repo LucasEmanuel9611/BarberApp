8b60ee4beec69bfeff5257481ce975f9f4fefdea
Lucas Emanuel <lucas190717@gmail.com>
2023-05-22 17:47:04 -0300

    feat: add styledc-components

--- App.tsx
+++ App.tsx
@@ -1,16 +1,18 @@
 import { StatusBar } from 'react-native';
+import { ThemeProvider } from 'styled-components/native';
 import { Routes } from './src/routes';
+import theme from './src/theme';
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
       <Routes />
-    </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme/index.ts
@@ -0,0 +1,23 @@
+export default {
+  COLORS: {
+    WHITE: "#EDF2F7",
+
+    ORANGE_100: "#FF9000",
+    ORANGE_200: "#F6AD55",
+    ORANGE_300: "#FBD38D",
+
+    GRAY_700: "#121214",
+    GRAY_600: "#202024",
+    GRAY_500: "#29292E",
+    GRAY_400: "#312E38",
+    GRAY_300: "#7C7C8A",
+    GRAY_200: "#C4C4CC",
+    GRAY_100: "#f4ede8",
+  },
+  FONT_SIZE: {
+    SM: 14,
+    MD: 16,
+    LG: 18,
+    XL: 24,
+  },
+};
